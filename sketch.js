2fc1c6487b46e46d3985e0aefc79e3f560cbaa59
ThisIsSebi <shoff439@gmail.com>
2025-03-20 20:50:36 +0100

    Adjusted scetch to change its colour using an if condition

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 var on = false;
+var x = 300;
 
 function setup() {
   createCanvas(600, 400);
@@ -6,22 +7,46 @@ function setup() {
 
 function draw() {
   background(0);
-  if(on){
-    background(0,255,0)
-  } else{
+  if(on === true){
     background(255,0,0)
+  } else{
+    background(0,255,0)
   }
   stroke(255);
   strokeWeight(4);
-  noFill();
+  fill(0,0,255);
+  if(mouseX > 250 && mouseX < 350 && mouseY > 150 && mouseY < 250){
+    fill(255,0,200)
+  }
   rectMode(CENTER)
-  rect(300,200,100,100)
-
+  rect(x,200,100,100, 50)
 }
+
+function mousePressed(){
+  if(mouseX > 250 && mouseX < 350 && mouseY > 150 && mouseY < 250){
+    if(on == true){
+       on = false;
+       } else{
+         on = true;
+       }  }
+}
+
+// function mousePressed(){
+//   if(mouseX > 350 && mouseX < 450 && mouseY > 150 && mouseY < 250){
+//     on = !on
+//     x = 300;
+//    }
+// }
 
 // if(mouseX > 250 && mouseX < 350 && mouseY > 150 && mouseY < 250){
 //   if(mouseIsPressed){
 //     background(0,255,0)
 //   }
 //   fill(255,0,200)
+// }
+
+// if(on){
+//   on = false;
+// } else{
+//   on = true;
 // }
