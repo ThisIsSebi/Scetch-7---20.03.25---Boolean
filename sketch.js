333ef3f7b115557f1c4fce34f52a99b07e38c804
ThisIsSebi <shoff439@gmail.com>
2025-03-23 12:39:37 +0100

    Added random Circle

--- sketch.js
+++ sketch.js
@@ -5,50 +5,62 @@ var y = 200;
 function setup() {
   createCanvas(600, 400);
 
-  let buttonLeft = createButton('left');
+  let buttonLeft = createButton("left");
   buttonLeft.mousePressed(turnLeft);
 
-  let buttonTop = createButton('top');
+  let buttonTop = createButton("top");
   buttonTop.mousePressed(turnUp);
 
-  let buttonDown = createButton('down');
+  let buttonDown = createButton("down");
   buttonDown.mousePressed(turnDown);
 
-  let buttonRight = createButton('right');
+  let buttonRight = createButton("right");
   buttonRight.mousePressed(turnRight);
+
 }
 
 function draw() {
   background(0);
-  if(on === true){
-    background(255,0,0)
-  } else{
-    background(0,255,0)
+  if (on === true) {
+    background(255, 0, 0);
+  } else {
+    background(0, 255, 0);
   }
   stroke(255);
   strokeWeight(4);
-  fill(0,0,255);
+  fill(0, 0, 255);
   // if(mouseX > 250 && mouseX < 350 && mouseY > 150 && mouseY < 250){
   //   fill(255,0,200)
   // }
-  rectMode(CENTER)
-  rect(x,y,100,100, 50)
-  console.log(mouseX)
+  rectMode(CENTER);
+
+  rect(x, y, 100, 100, 50);
+  console.log(mouseX);
+
+  let randomX = random(0, 600);
+  let randomY = random(0, 400);
+
+  randomCircle(randomX, randomY);
 }
 
-function turnRight(){
+function randomCircle(randomX, randomY){
+  frameRate(1)
+  circle(randomX, randomY, 10,10)
+}
+
+function turnRight() {
   x += 100;
 }
 
-function turnLeft(){
+function turnLeft() {
   x -= 100;
 }
 
-function turnUp(){
+function turnUp() {
   y -= 100;
 }
 
-function turnDown(){
+function turnDown() {
   y += 100;
 }
 
@@ -58,7 +70,7 @@ function turnDown(){
 //   //      on = false;
 //   //      } else{
 //   //        on = true;
-//   //      } 
+//   //      }
 //   //      console.log(x)
 //   //  }
 //   }
